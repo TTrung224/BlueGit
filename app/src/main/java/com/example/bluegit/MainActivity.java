05372f25ee9f380df6cac71a105f1e6fdb9769f9
cupOWater <something2805@gmail.com>
2023-01-05 19:24:45 +0700

    change to FireBase FireStore

--- app/src/main/java/com/example/bluegit/MainActivity.java
+++ app/src/main/java/com/example/bluegit/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.bluegit;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
     }
 }
