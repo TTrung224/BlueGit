8045eed8cf1cf995016ae44287e0fa409abd6c8d
cupOWater <something2805@gmail.com>
2023-01-06 17:06:20 +0700

    add Login Activity UI

---
+++ app/src/main/java/com/example/bluegit/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.bluegit;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
