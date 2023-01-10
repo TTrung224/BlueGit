c862b88f29a22085f2abbf980500452a1ff0e0be
s3877653 <hotranminhkhoi@gmail.com>
2023-01-10 23:22:31 +0700

    add admin product adapter

--- app/src/main/java/com/example/bluegit/AdminProductActivity.java
+++ app/src/main/java/com/example/bluegit/AdminProductActivity.java
@@ -1,14 +1,34 @@
 package com.example.bluegit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.bluegit.adapters.AdminProductAdapter;
+import com.example.bluegit.adapters.OrderBuyAdapter;
+import com.example.bluegit.model.Product;
+
+import java.util.ArrayList;
+
 public class AdminProductActivity extends AppCompatActivity {
 
+    private ArrayList<Product> products;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_product);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // TODO: retrieve and process data here
+
+        RecyclerView recyclerView = findViewById(R.id.adminProductList);
+        AdminProductAdapter adapter = new AdminProductAdapter(products, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/example/bluegit/adapters/AdminProductAdapter.java
+++ app/src/main/java/com/example/bluegit/adapters/AdminProductAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,34 +17,37 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-public class ProductDisplayAdapter extends RecyclerView.Adapter<ProductDisplayAdapter.ViewHolder>{
-
+public class AdminProductAdapter extends RecyclerView.Adapter<AdminProductAdapter.ViewHolder>{
     private ArrayList<Product> products;
     private LayoutInflater inflater;
 
-    public ProductDisplayAdapter(ArrayList<Product> products, Context context) {
+    public AdminProductAdapter(ArrayList<Product> products, Context context) {
         this.products = products;
         this.inflater = LayoutInflater.from(context);
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder{
         public ImageView productImage;
         public TextView productName;
         public TextView productPrice;
+        public TextView productOwner;
+        public Button productDeleteBtn;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            productImage = itemView.findViewById(R.id.display_product_image);
-            productName = itemView.findViewById(R.id.display_product_name);
-            productPrice = itemView.findViewById(R.id.display_product_price);
+            productImage = itemView.findViewById(R.id.adminProductImageItem);
+            productName = itemView.findViewById(R.id.adminProductNameItem);
+            productPrice = itemView.findViewById(R.id.adminProductPriceItem);
+            productOwner = itemView.findViewById(R.id.adminProductSellerItem);
+            productDeleteBtn = itemView.findViewById(R.id.adminProductDeleteItem);
         }
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.recyclerview_item_display, parent, false);
+        View view = inflater.inflate(R.layout.layout_product_admin, parent, false);
         return new ViewHolder(view);
     }
 
@@ -53,12 +57,13 @@ public class ProductDisplayAdapter extends RecyclerView.Adapter<ProductDisplayAd
         holder.productName.setText(products.get(position).getProductName());
         String price = products.get(position).getProductPrice() + " đ";
         holder.productPrice.setText(price);
+//        holder.productOwner.setText(products.get(position).getsellerId());
     }
+
+
 
     @Override
     public int getItemCount() {
         return products.size();
     }
-
-
 }
