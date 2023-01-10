f8ef0d7f69309006a06c5bbc38f82c71c140869d
Trung Tran <s3891724@rmit.edu.vn>
2023-01-10 11:50:44 +0700

    finish bottom nav

--- app/src/main/java/com/example/bluegit/CartActivity.java
+++ app/src/main/java/com/example/bluegit/CartActivity.java
@@ -2,13 +2,62 @@ package com.example.bluegit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class CartActivity extends AppCompatActivity {
+    private String uId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        uId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+
+    }
+
+    public void goBack(View view) {
+        setResult(RESULT_OK);
+        finish();
+    }
+
+    public void toBusiness(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("navTo", MainActivity.NAV_TO_BUSINESS);
+        setResult(RESULT_OK, intent);
+        finish();
+        overridePendingTransition(0, 0);
+    }
+
+    public void toOrders(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("navTo", MainActivity.NAV_TO_ORDERS);
+        setResult(RESULT_OK, intent);
+        finish();
+        overridePendingTransition(0, 0);
+    }
+
+    public void toHome(View view) {
+        setResult(RESULT_OK);
+        finish();
+        overridePendingTransition(0, 0);
+    }
+
+    public void refresh(View view) {
+        this.recreate();
+    }
+
+    public void toAccount(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("navTo", MainActivity.NAV_TO_ACCOUNT);
+        setResult(RESULT_OK, intent);
+        finish();
+        overridePendingTransition(0, 0);
     }
 }
--- app/src/main/java/com/example/bluegit/OrderBuyDetailActivity.java
+++ app/src/main/java/com/example/bluegit/OrderBuyDetailActivity.java
@@ -1,12 +1,23 @@
 package com.example.bluegit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.bluegit.adapters.OrderBuyAdapter;
+import com.example.bluegit.adapters.OrderProductAdapter;
+import com.example.bluegit.model.Order;
+import com.example.bluegit.model.Product;
+
+import java.util.ArrayList;
+
 public class OrderBuyDetailActivity extends AppCompatActivity {
+    ArrayList<Product> productList = new ArrayList<>();
+    ArrayList<Integer> quantityList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +25,17 @@ public class OrderBuyDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order_buy_detail);
         TextView oldPrice = findViewById(R.id.totalPriceBeforeDiscount);
         oldPrice.setPaintFlags(oldPrice.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // TODO: retrieve and  process data here
+
+        RecyclerView recyclerView = findViewById(R.id.orderProductList);
+        OrderProductAdapter adapter = new OrderProductAdapter(productList, quantityList, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
