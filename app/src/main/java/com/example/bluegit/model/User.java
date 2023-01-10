5ba999e5f917875b5366b786311077a77cccfd49
cupOWater <something2805@gmail.com>
2023-01-10 14:53:44 +0700

    Remove IgnoreExtraProperties in User

--- app/src/main/java/com/example/bluegit/model/User.java
+++ app/src/main/java/com/example/bluegit/model/User.java
@@ -2,7 +2,7 @@ package com.example.bluegit.model;
 
 import android.net.Uri;
 
-import org.threeten.bp.LocalDateTime;
+import com.google.firebase.firestore.Exclude;
 
 import java.util.ArrayList;
 
@@ -13,30 +13,9 @@ public class User {
     private Uri profileImageSrc;
     private String email;
     private String phoneNumber;
-    
-    // Account
-    private String name;
-    private int age;
-    private String phone;
-    private String address;
 
-    // Seller
-    private int numCompleted;
-    private int numDelivery;
-    private int numConfirmed;
-    private int numPending;
-
+    @Exclude
     private ArrayList<Product> products;
-    private ArrayList<Integer> amount;
-    private float totalPrice;
-    private String voucher;
-    private LocalDateTime createdDate;
-    private String status; // preparing delivering received
-    private int totalProductQuantity;
-
-    // TODO: reconsider data type
-    private String customerId;
-    private String sellerId;
 
     public User() {
     }
@@ -47,44 +26,6 @@ public class User {
         this.displayName = displayName;
         this.email = email;
         this.profileImageSrc = profileImageSrc;
-    }
-
-    // Account
-    public User(Uri profileImgSrc, String displayName, String name, int age, String phone, String address) {
-        this.name = name;
-        this.displayName = displayName;
-        this.age = age;
-        this.phone = phone;
-        this.address = address;
-        this.profileImageSrc = profileImgSrc;
-    }
-
-    // Seller
-    public User(String id, ArrayList<Product> products, ArrayList<Integer> amount,
-                  String voucher, String customerId, String sellerId) {
-        this.id = id;
-        this.products = products;
-        this.amount = amount;
-        this.voucher = voucher;
-        this.customerId = customerId;
-        this.sellerId = sellerId;
-
-        this.status = "preparing";
-
-        createdDate = LocalDateTime.now();
-
-        float price = 0;
-        for(int i = 0; i < products.size(); i++){
-            price += products.get(i).getProductPrice()*amount.get(i);
-        }
-        this.totalPrice = price;
-
-
-        int quantity = 0;
-        for(int i: amount){
-            quantity += i;
-        }
-        this.totalProductQuantity = quantity;
     }
 
 
