30d27550bd292b2cbb2a15d5b14171dd1fdb411f
s3877653 <hotranminhkhoi@gmail.com>
2023-01-11 17:46:13 +0700

    khoi commit

--- app/src/main/java/com/example/bluegit/AdminAccountActivity.java
+++ app/src/main/java/com/example/bluegit/AdminAccountActivity.java
@@ -1,14 +1,32 @@
 package com.example.bluegit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.accounts.Account;
 import android.os.Bundle;
 
-public class AdminAccountActivity extends AppCompatActivity {
+import com.example.bluegit.adapters.AdminProductAdapter;
 
+import java.util.ArrayList;
+
+public class AdminAccountActivity extends AppCompatActivity {
+    private ArrayList<Account> accounts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_account);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // TODO: retrieve and process data here
+
+        RecyclerView recyclerView = findViewById(R.id.adminAccountList);
+        AdminProductAdapter adapter = new AdminProductAdapter(accounts, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/example/bluegit/CartActivity.java
+++ app/src/main/java/com/example/bluegit/CartActivity.java
@@ -1,17 +1,23 @@
 package com.example.bluegit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.bluegit.adapters.AdminProductAdapter;
+import com.example.bluegit.model.Product;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class CartActivity extends AppCompatActivity {
     private String uId;
+    private ArrayList<Product> products;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,17 @@ public class CartActivity extends AppCompatActivity {
 
         uId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
 
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // TODO: retrieve and process data here
+
+        RecyclerView recyclerView = findViewById(R.id.itemList);
+        AdminProductAdapter adapter = new AdminProductAdapter(products, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     public void goBack(View view) {
